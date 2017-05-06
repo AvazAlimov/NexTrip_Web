f2c3b5d9a8bbd965e74da06c67893b91b51c1df9
Asus <avazalimov95@mail.ru>
2017-05-06 12:54:20 +0500

    Avaz Commited

---
+++ classes/ThingsToDo.php
@@ -0,0 +1,59 @@
+<?php
+
+class ThingsToDo extends Guidance
+{
+    private $startDate;
+    private $endDate;
+    private $price;
+    private $rules;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function toString()
+    {
+        return parent::toString() . $this->startDate . "◎" . $this->endDate . "◎" . $this->price . "◎" . $this->rules;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+    }
+
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    public function setRules($rules)
+    {
+        $this->rules = $rules;
+    }
+}
