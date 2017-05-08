df17ffc16ed1f6e736c03f6f204881046f9e2334
Asus <avazalimov95@mail.ru>
2017-05-08 18:39:29 +0500

    Avaz Commited

--- index.php
+++ index.php
@@ -138,7 +138,7 @@
 <body>
 <img id="logo" src="src/logo.gif" alt="NexTrip"/>
 <div class="textContainer">
-    <a href="HTML/bootstrap1.html" style="text-decoration: none;"><h1 id="text" class="text"></h1></a>
+    <a href="HTML/main.php" style="text-decoration: none;"><h1 id="text" class="text"></h1></a>
 </div>
 </body>
 </html>
